71d0b65fe73934d42ac351771b6ec94a0a519c61
SHL <huailishang@163.com>
2026-07-30 17:19:04 +0800

    feat: add P1 confirmation binding gate

--- src/agentic_payment_experiment/trusted_execution/__init__.py
+++ src/agentic_payment_experiment/trusted_execution/__init__.py
@@ -6,6 +6,16 @@ or lifecycle policy.
 """
 
 from .binding import BindingResult, BindingStatus, verify_binding
+from .confirmation import (
+    ConfirmationBindingFact,
+    ConfirmationRecord,
+    ConfirmationStatus,
+    PaymentGateOutcome,
+    confirmation_order_payload,
+    create_confirmation_record,
+    execute_with_confirmation_gate,
+    verify_confirmation_binding,
+)
 from .execution_facts import (
     ExecutionAttemptFact,
     IdempotencyFact,
@@ -21,6 +31,10 @@ from .hashing import canonical_hash, canonical_json, canonicalize, verify_hash
 __all__ = [
     "BindingResult",
     "BindingStatus",
+    "ConfirmationBindingFact",
+    "ConfirmationRecord",
+    "ConfirmationStatus",
+    "PaymentGateOutcome",
     "ExecutionAttemptFact",
     "IdempotencyFact",
     "VerificationResult",
@@ -28,9 +42,13 @@ __all__ = [
     "canonicalize",
     "canonical_json",
     "canonical_hash",
+    "confirmation_order_payload",
+    "create_confirmation_record",
+    "execute_with_confirmation_gate",
     "check_idempotency",
     "validate_status_observation",
     "verify_binding",
+    "verify_confirmation_binding",
     "verify_declared_identity_binding",
     "verify_execution_identity",
     "verify_hash",
